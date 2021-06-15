f72beec0d03dad4b242958b0ae05028b4ee3fa15
MaksymOp <40294556+jilimb0@users.noreply.github.com>
2021-06-15 11:04:07 +0300

    ver 1.1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import "./App.css"
+import "./App.scss"
 
 import axios from "axios"
 
-import AddFilm from "./components/AddFilm/index.js"
+// import AddFilm from "./components/AddFilm/index.js"
 import { useEffect, useState } from "react"
+
+function useForceUpdate() {
+  const [value, setValue] = useState(0)
+  return () => setValue((value) => value + 1)
+}
 
 function App() {
   const [MOVIES, setMovies] = useState([])
+
+  const forceUpdate = useForceUpdate()
+
+  const [searchTerm, setSearchTerm] = useState("")
 
   async function getMovies() {
     setMovies(
@@ -17,6 +26,14 @@ function App() {
           throw e
         })
     )
+  }
+
+  function sortByAlphabet() {
+    const sortedMovies = MOVIES.sort((a, b) => (a.Title > b.Title ? 1 : -1))
+
+    setMovies(sortedMovies)
+
+    forceUpdate()
   }
 
   /** Troubles with data sending (empty body) */
@@ -46,8 +63,7 @@ function App() {
 
   return (
     <div className="App">
-      <header>
-        {/* <button
+      {/* <button
           className="addFilm"
           onClick={() => {
             postMovies(
@@ -62,35 +78,78 @@ function App() {
           ADD
         </button> */}
 
-        <button>Sort by Alphabet</button>
-        <input
-          type="search"
-          name="Search"
-          id=""
-          placeholder="Title or actor's name..."
-        />
-      </header>
-      <section>
-        {MOVIES.map(
-          ({
-            id,
-            Title: title,
-            "Release Year": year,
-            Format: format,
-            Stars: stars,
-          }) => {
-            return (
-              <div className="movie" key={id}>
-                <div className="filmId">Film ID: {id}</div>
-                <div className="title">Title: {title}</div>
-                <div className="year">Release year: {year}</div>
-                <div className="format">Format: {format}</div>
-                <div className="stars">Film stars: {stars}</div>
-              </div>
-            )
-          }
-        )}
-      </section>
+      <main>
+        <div className="wrap-search">
+          <button onClick={() => sortByAlphabet()}>Sort by Alphabet</button>
+          <input
+            name="search"
+            type="text"
+            maxLength="30"
+            placeholder="Type here title or actor's name..."
+            value={searchTerm}
+            onChange={(e) => {
+              setSearchTerm(e.target.value)
+            }}
+          />
+        </div>
+        <div className="item-list">
+          {searchTerm === ""
+            ? MOVIES.map(
+                ({
+                  id,
+                  Title: title,
+                  "Release Year": year,
+                  Format: format,
+                  Stars: stars,
+                }) => {
+                  return (
+                    <div className="movie" key={id}>
+                      <div className="title">Title: {title}</div>
+                      <div className="year">Release year: {year}</div>
+                      <div className="format">Format: {format}</div>
+                      <div className="stars">Film stars: {stars}</div>
+                      <div className="filmId">Film ID: {id}</div>
+                    </div>
+                  )
+                }
+              )
+            : MOVIES.filter(
+                ({
+                  id,
+                  Title: title,
+                  "Release Year": year,
+                  Format: format,
+                  Stars: stars,
+                }) => {
+                  return (
+                    title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                    stars.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                    format.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                    year.toString().includes(searchTerm.toString()) ||
+                    id.toString().includes(searchTerm.toString())
+                  )
+                }
+              ).map(
+                ({
+                  id,
+                  Title: title,
+                  "Release Year": year,
+                  Format: format,
+                  Stars: stars,
+                }) => {
+                  return (
+                    <div className="movie" key={id}>
+                      <div className="title">Title: {title}</div>
+                      <div className="year">Release year: {year}</div>
+                      <div className="format">Format: {format}</div>
+                      <div className="stars">Film stars: {stars}</div>
+                      <div className="filmId">Film ID: {id}</div>
+                    </div>
+                  )
+                }
+              )}
+        </div>
+      </main>
     </div>
   )
 }
